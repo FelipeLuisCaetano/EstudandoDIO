6d1aca006dadc87e3f25203d59a277cd1e7749cf
FelipeLuisCaetano <98624218+FelipeLuisCaetano@users.noreply.github.com>
2022-01-30 23:09:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProjetoRPG.src.Entities;
+
+Knight arus = new Knight("Arus", 23, "Knight");
+Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+Wizard bruxao = new Wizard("Geralt", 90, "Bruxo");
+ 
+
+Console.WriteLine(arus.Attack());
+Console.WriteLine(wizard.Attack());
+Console.WriteLine(wizard.Attack(5));
+Console.WriteLine(bruxao.Attack(8));
+
